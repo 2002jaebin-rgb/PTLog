5d0889b7dd14b31c27d4a1a404f5718282e883cc
2002jaebin-rgb <2002jaebin@gmail.com>
2025-10-04 19:06:06 +0900

    setup

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+// /src/main.jsx
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+const rootEl = document.getElementById('root')
+createRoot(rootEl).render(<App />)
