6b4f6b96d2c3c4cfeaa5dd33aadb64d138cd2e35
2002jaebin-rgb <2002jaebin@gmail.com>
2025-10-07 16:14:23 +0900

    Update AddSessionModal.jsx

--- src/components/AddSessionModal.jsx
+++ src/components/AddSessionModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { supabase } from '@supabaseClient'
+import { supabase } from '@/supabaseClient'
 import ScheduleGrid from './ScheduleGrid'
 import Button from './ui/Button'
 import Card from './ui/Card'
